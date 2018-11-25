ab01dab165841b50c006c986bb7f571a512f9727
Maheshchoda <choda.maheshbabu@gmail.com>
2018-11-25 10:11:47 +0530

    fixed port issue

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 2018;
 
 app.get("/", (req, res) => {
   res.json({ message: `Hello there baby` });
 });
 
-const PORT = process.env.port || 2018;
 app.listen(PORT, err => {
   if (err) console.log(`There is some problem with server`);
   console.log(`You server is started on port number ${PORT}`);
