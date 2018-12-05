12fb8f6a0cb5ec92fa885b18f603dff2f64f99ff
Maheshchoda <choda.maheshbabu@gmail.com>
2018-12-05 09:58:10 +0530

    production

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,19 @@
 module.exports = {
-  cookieKey: process.env.COOKIE_KEY,
-  clientID: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
-  PublishableKey: process.env.PUBLISHABLE_KEY,
-  SecretKey: process.env.SECRET_KEY,
-  sendGridKey: process.env.SEND_GRID_KEY
+  cookie: {
+    cookieKey: process.env.COOKIE_KEY
+  },
+  google: {
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  },
+  db: {
+    mongoURI: process.env.MONGO_URI
+  },
+  stripe: {
+    PublishableKey: process.env.PUBLISHABLE_KEY,
+    SecretKey: process.env.SECRET_KEY
+  },
+  sendGrid: {
+    sendGridKey: process.env.SEND_GRID_KEY
+  }
 };
