bc5680551335e92044e8146ad9e16818d71042a1
Maheshchoda <choda.maheshbabu@gmail.com>
2018-12-05 09:43:12 +0530

    changes in production keyset

--- config/prod.js
+++ config/prod.js
@@ -1,16 +1,9 @@
 module.exports = {
-  cookie: {
-    cookieKey: process.env.COOKIE_KEY
-  },
-  google: {
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
-  },
-  db: {
-    mongoURI: process.env.MONGO_URI
-  },
-  stripe: {
-    PublishableKey: process.env.Publishable_Key,
-    SecretKey: process.env.Secret_Key
-  }
+  cookieKey: process.env.COOKIE_KEY,
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  PublishableKey: process.env.Publishable_Key,
+  SecretKey: process.env.Secret_Key,
+  sendGridKey: process.env.sendGridKey
 };
