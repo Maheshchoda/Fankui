8d134229276d33436fce14f0ac0b91da8de3f00c
Maheshchoda <choda.maheshbabu@gmail.com>
2018-12-08 11:08:11 +0530

    deploy check

--- config/prod.js
+++ config/prod.js
@@ -1,22 +1,10 @@
 module.exports = {
-  cookie: {
-    cookieKey: process.env.COOKIE_KEY
-  },
-  google: {
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
-  },
-  db: {
-    mongoURI: process.env.MONGO_URI
-  },
-  stripe: {
-    PublishableKey: process.env.PUBLISHABLE_KEY,
-    SecretKey: process.env.SECRET_KEY
-  },
-  sendGrid: {
-    sendGridKey: process.env.SEND_GRID_KEY
-  },
-  redirectDomain: {
-    domain: process.env.REDIRECT_DOMAIN
-  }
+  cookieKey: process.env.COOKIE_KEY,
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  PublishableKey: process.env.PUBLISHABLE_KEY,
+  SecretKey: process.env.SECRET_KEY,
+  sendGridKey: process.env.SEND_GRID_KEY,
+  domain: process.env.REDIRECT_DOMAIN
 };
