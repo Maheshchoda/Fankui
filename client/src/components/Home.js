fb43e42a143e1a43bd54b6fa220ac931da91d009
Maheshchoda <choda.maheshbabu@gmail.com>
2018-12-08 12:05:11 +0530

    production changes

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import homeImage from "./group-1825513_1280.jpg";
 
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to the Home page</h1>
+      <h1 className="center">We take care of your feedback</h1>
+      <img src={homeImage} width="100%" height="100%" alt="feedback" />
     </div>
   );
 };
